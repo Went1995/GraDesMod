9c0b6e7949690d783e18e73b9ef7636ef3910805
姜文韬 <1106892433@qq.com>
2016-09-10 15:12:42 +0800

    aa

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,35 @@
-/**
- * Created by 姜文韬 on 2016/9/7.
- */
+///**
+// * Created by 姜文韬 on 2016/9/8.
+// */
+//function click1(){
+//    var box1 = document.getElementById('#box1');
+//    box1.setAttribute("display","block");
+//}
+//function click2(){
+//    var box2 = document.getElementById('#box1');
+//    box2.setAttribute("display","block");
+//}
 
+$(function() {
+    $("#box2 div span").click(function(){
+        $("#box1").css("float","left");
+        $("#box1").show(1000);
+    });
+    $("#box2").click(function(){
+        $("#box1").css("float","left");
+        $("#box1").hide(1000);
+    });
+    $("#btn1").click(function(){
+        $("#btn2").removeClass("active");
+        $("#btn1").addClass("active");
+    });
+    $("#btn2").click(function(){
+        $("#btn1").removeClass("active");
+        $("#btn2").addClass("active");
+    });
+    //$("#box1").toggle(function(){
+    //
+    //},function(){
+    //
+    //});
+})
